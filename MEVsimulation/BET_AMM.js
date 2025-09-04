9c466ef63386d212f948db011e8ed7d68f5a7a6e
Semia Guesmi <semiaguesmi@Semias-MacBook-Pro.local>
2025-09-04 17:18:00 +0200

    create objects for deployed contract

--- MEVsimulation/BET_AMM.js
+++ MEVsimulation/BET_AMM.js
@@ -1,14 +1,27 @@
 const { 
        BetContract_Address,
-       Exchange_Address,
        TestToken_Address,
        AMM_Address,
        BetContract_Abi,
        TestToken_Abi,
-       Exchange_Abi,
        AMM_Abi } = require('./constant');
-(async () => {
-    console.log("API_KEY:", process.env.API_KEY);
-    console.log("PRIVATE_KEY:", process.env.PRIVATE_KEY);
+
+       const getEthereumContract = (address, abi, signer) => {
+       const TransactionContract = new ethers.Contract(address, abi, signer);
+       return TransactionContract
+      }
+ (async () => {
+
+    const alchemy = ethers.getDefaultProvider('sepolia', { alchemy: process.env.API_KEY });
+    const signer = new ethers.Wallet(process.env.PRIVATE_KEY);
+    const connectedWallet = signer.connect(alchemy);
+    const TestToken = getEthereumContract(TestToken_Address, TestToken_Abi, connectedWallet);
+    const AMM = getEthereumContract(AMM_Address, AMM_Abi, connectedWallet);
+    const BetContract = getEthereumContract(BetContract_Address, BetContract_Abi, connectedWallet);
+
+    console.log("TestToken  :  ", TestToken);
+    console.log("AMM object  :  ", AMM);
+    console.log("BetContract object  :  ", BetContract);
+
   
   })();
