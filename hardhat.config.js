a45dfc4fffc95c831975998b20bf3d6a96d7917d
Semia Guesmi <semiaguesmi@Semias-MacBook-Pro.local>
2025-09-04 10:18:02 +0200

    deployment configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url:"https://eth-sepolia.g.alchemy.com/v2/YOUR_API_KEY",
+      accounts: ["YOUR PRIVATE KEY"],
+      chainId: 11155111,
+    }
+  }
 };
